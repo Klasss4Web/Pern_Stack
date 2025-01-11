6c5f506fef72d44e67e482ca25c8343db20a74e7
emmanuel.ochade <emmanuel.ochade@interswitchng.com>
2025-01-11 14:42:24 +0100

    refactored the delete end point to handle bad requests

--- src/students/controllers.js
+++ src/students/controllers.js
@@ -51,18 +51,20 @@ const addStudent = (req, res) => {
 
 const deleteStudentById = (req, res) => {
   const studentId = parseInt(req.params.id);
-  pool.query(removeStudentById, [studentId], (error, results) => {
-    console.log({ results });
+  pool.query(studentById, [studentId], (error, results) => {
     if (error) throw new Error("error", error);
-    // if (results.rows.length < 1)
-    //   return res.status(400).json({
-    //     message: "Student not found",
-    //     success: false,
-    //   });
+    if (!results.rows.length)
+      res.status(400).json({
+        message: "Student not found",
+        success: false,
+      });
 
-    res.status(200).json({
-      message: "Student deleted successfully!",
-      success: true,
+    pool.query(removeStudentById, [studentId], (error, results) => {
+      if (error) throw new Error("error", error);
+      res.status(200).json({
+        message: "Student deleted successfully!",
+        success: true,
+      });
     });
   });
 };
